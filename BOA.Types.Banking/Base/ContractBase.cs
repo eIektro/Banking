57f6607ba3d8a466b13d7737b753ed6d4576fd05
M. Emre Koral <elektronikotin@gmail.com>
2021-01-13 01:51:50 +0300

    name convention changes

---
+++ BOA.Types.Banking/Base/ContractBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BOA.Types.Banking.Base
+{
+    public class ContractBase : INotifyPropertyChanged
+    {
+
+        private readonly Dictionary<string, object> _values = new Dictionary<string, object>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //[NotifyPropertyChangedInvocator]
+        public virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public T GetProperty<T>([CallerMemberName] string propertyName = "")
+        {
+            EnsureElement<T>(propertyName);
+            return (T)_values[propertyName];
+        }
+
+        
+        public void SetProperty<T>(object value, [CallerMemberName] string propertyName = "")
+        {
+            EnsureElement<T>(propertyName);
+            if (_values[propertyName] == value)
+            {
+                return;
+            }
+            _values[propertyName] = value;
+        }
+
+        private void EnsureElement<T>(string propertyName)
+        {
+            if (!_values.ContainsKey(propertyName))
+            {
+                _values.Add(propertyName, default(T));
+            }
+        }
+    }
+    
+}
